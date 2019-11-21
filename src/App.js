2c6590863618c8792f1df0c719f25183066a100c
Shraddha <kudalkarshraddha99@gmail.com>
2019-11-21 11:07:07 +0530

    Use of props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
-      
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
     </div>
   );
 }
