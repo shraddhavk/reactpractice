3ee76ffbc8d2cede7c4e9586920d0057d62b7e26
Shraddha <kudalkarshraddha99@gmail.com>
2019-11-21 11:17:59 +0530

    Use of props eg 3

--- src/App.js
+++ src/App.js
@@ -9,11 +9,7 @@ function Comment(props) {
   return (
     <div className="Comment">
       <div className="UserInfo">
-        <img
-          className="Avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
+      <Avatar user={props.author} />
         <div className="UserInfo-name">
           {props.author.name}
         </div>
@@ -23,6 +19,15 @@ function Comment(props) {
         {formatDate(props.date)}
       </div>
     </div>
+  );
+}
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+
   );
 }
 
