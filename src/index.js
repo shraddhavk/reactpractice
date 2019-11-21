4d7c551a4076f6ad1d253ef0d4407fe055a3a499
Shraddha <kudalkarshraddha99@gmail.com>
2019-11-21 10:14:20 +0530

    JSX 2

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const name = 'Shraddha Kudalkar'
-const element = <h1>Hello {name} </h1>
+function formatName(user) {
+    return user.firstname + ' ' + user.lastname;
+}
+
+const user = {
+    firstname :'Shraddha',
+    lastname:'Kudalkar'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
 
 ReactDOM.render(element, document.getElementById('root'));
 
