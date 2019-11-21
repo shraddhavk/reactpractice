b4a6cd9464614265ce4c6b644d5e3301ab3b0772
Shraddha <kudalkarshraddha99@gmail.com>
2019-11-21 11:15:35 +0530

    Use of props eg 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img
+          className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
 
 function App() {
   return (
     <div className="App">
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+      <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author}
+      />
     </div>
   );
 }
