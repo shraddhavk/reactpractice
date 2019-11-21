ebb7042065c05ece0e353bbcfe4e532fc3ce61df
Shraddha <kudalkarshraddha99@gmail.com>
2019-11-21 13:18:58 +0530

    Select form

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import LoginControl from './LoginControl';
 import NameForm from './nameform';
+import EssayForm from './EssayForm';
+import FlavorForm from './FlavorForm';
 
   ReactDOM.render(
-    <NameForm />,
+    <FlavorForm />,
     document.getElementById('root')
   );
 serviceWorker.unregister();
