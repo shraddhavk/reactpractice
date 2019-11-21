d5fa321de901b846111788ecb184493a5e0b5858
Shraddha <kudalkarshraddha99@gmail.com>
2019-11-21 12:05:42 +0530

    Clock with state and props

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
 import React from 'react';
 import './App.css';
+import Clock from './clock';
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <div className="UserInfo">
-      <Avatar user={props.author} />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
-      <div className="Comment-text">{props.text}</div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-function Avatar(props) {
-  return (
-    <img className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
-
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'https://placekitten.com/g/64/64',
-  },
-};
 
 function App() {
   return (
-    <div className="App">
-      <Comment
-      date={comment.date}
-      text={comment.text}
-      author={comment.author}
-      />
+    <div>
+      <Clock />
+      <Clock />
+      <Clock />
     </div>
   );
 }
+
 
 export default App;
